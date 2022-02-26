a801f9b086c4b790dd2ab35b21cc5ce07f9e9807
Miguel Toro <miguel.toro1985@gmail.com>
2022-02-26 11:28:03 -0300

    primer commit

---
+++ sqlapp/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using sqlapp.Models;
+using sqlapp.Services;
+
+namespace sqlapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly CourseService _course_service;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, CourseService _svc, IConfiguration _config)
+        {
+            _logger = logger;
+            _course_service = _svc;
+            _configuration = _config;
+        }
+
+        public IActionResult Index()
+        {
+            //IEnumerable<Course> _course_list = _course_service.GetCourses(_configuration.GetConnectionString("SqlConnection"));
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ sqlapp/Services/CourseService.cs
@@ -0,0 +1,58 @@
+﻿using sqlapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sqlapp.Services
+{
+    public class CourseService
+    {
+        private static string db_source = "az204-sqldatabase-sql.database.windows.net";
+        private static string db_user = "mtoroc";
+        private static string db_password = "Toroku1985..";
+        private static string db_database = "az204-sqldatabase-sqldb";
+
+        private SqlConnection GetConnection(string _connection_string)
+        {
+            //var builder = new SqlConnectionStringBuilder();
+            //builder.DataSource = db_source;
+            //builder.UserID = db_user;
+            //builder.Password = db_password;
+            //builder.InitialCatalog = db_database;
+
+            return new SqlConnection(_connection_string);
+        }
+
+        public IEnumerable<Course> GetCourses(string _connection_string)
+        {
+            List<Course> _lst = new List<Course>();
+            string _statement = "SELECT * FROM Course;";
+            SqlConnection _connection = GetConnection(_connection_string);
+            _connection.Open();
+            SqlCommand _sqlCommand = new SqlCommand(_statement, _connection);
+
+            using (SqlDataReader _reader = _sqlCommand.ExecuteReader())
+            {
+
+                while (_reader.Read())
+                {
+
+                    Course _course = new Course()
+                    {
+                        CourseID = _reader.GetInt32(0),
+                        CourseName = _reader.GetString(1),
+                        Rating = _reader.GetDecimal(2)
+                    };
+
+                    _lst.Add(_course);
+                }
+            }
+
+            _connection.Close();
+
+            return _lst;
+        }
+    }
+}
